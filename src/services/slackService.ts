73329f5abe26b4c79566c4664e036289ce11a142
Atharva-Kanherkar <atharvakanherkar25@gmail.com>
2024-07-20 13:11:59 +0530

    Slack works now

--- src/services/slackService.ts
+++ src/services/slackService.ts
@@ -6,13 +6,20 @@ dotenv.config();
 const token = process.env.SLACK_BOT_TOKEN;
 const slackClient = new WebClient(token);
 
-export const sendSlackMessage = async (channel: string, text: string) => {
+export const sendSlackMessage = async (channel: string, text: string): Promise<boolean> => {
   try {
-    await slackClient.chat.postMessage({
+    // Send the message
+    const result = await slackClient.chat.postMessage({
       channel,
       text,
     });
-  } catch (error) {
+    console.log('Message sent: ', result.ts);
+    return true;
+  } catch (error: unknown) {
     console.error('Error sending Slack message:', error);
+    if ((error as any).data) {
+      console.error('Slack API Error: ', (error as any).data);
+    }
+    return false;
   }
 };
