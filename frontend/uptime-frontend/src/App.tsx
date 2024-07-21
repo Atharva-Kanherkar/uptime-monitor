5058f4caafe500ecda88a1abe119f6b46537318c
Atharva-Kanherkar <atharvakanherkar25@gmail.com>
2024-07-22 01:26:15 +0530

    Added navbar

--- frontend/uptime-frontend/src/App.tsx
+++ frontend/uptime-frontend/src/App.tsx
@@ -1,16 +1,25 @@
 import { ThemeProvider } from "@/components/theme-provider"
 import { ModeToggle } from "./components/mode-toggle"
+import { Navbar } from "./pages/navbar";
+ 
+  
 
 function App() {
  
   return (
+    <>
+     
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-     <ModeToggle/>
-      <div className="w-full h-full bg-background text-primary-foreground">
-        <h1 className="text-4xl font-bold">Hello, world!</h1>
-    </div>
+      <>
+      <div className="mt-2">
+      <Navbar/>
+      </div>
+    
+      </>
     </ThemeProvider>
+    </>
   )
 }
 
-export default App
+export default App;
+
